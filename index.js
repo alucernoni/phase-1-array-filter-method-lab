7b807e4d7e220bcf8e1a9f4389646384cb4296e9
Annemarie <a.lucernoni0@gmail.com>
2022-04-29 19:48:43 -0700

    commit tree 56fe5482dee4d67c51c2f423e3a473c50e16a28e

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+
+
+function findMatching(collection, string) {
+    const driversArray = [];
+    for (const user of collection) {
+      if (string.toLowerCase() === user.toLowerCase()) {
+        driversArray.push(user)
+      }
+    }
+    return driversArray
+  }
+
+//   function fuzzyMatch(collection, string) {
+//     return collection.filter(function(letters) {
+//       return letters.toLowerCase().indexOf(string.toLowerCase()) !== -1
+//     })
+//   }
+
+function fuzzyMatch(collection, string) {
+    return collection.filter(function(letters) {
+      return letters.toLowerCase().substring(0,1).indexOf(string.toLowerCase().substring(0,1)) !== -1
+    })
+  }
+
+  function matchName(collection, name) {
+    return collection.filter(function(person) {
+      if (name === person.name) {
+        return true
+      }
+    })
+  }
